d657bfe1c40e8521c60c2e4323dc2a066a08ecb6
aadya1710 <aadya.mittal17@gmail.com>
2022-07-06 18:21:31 +0530

    frist commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public float speed;
+
+    void Start()
+    {
+        //print("Start");
+    }
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, -speed);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            rigidbody2d.velocity = new Vector2(0f, 0f);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Door")
+        {
+            Debug.Log("Level Completed!!!");
+        }
+        
+    }
+}
